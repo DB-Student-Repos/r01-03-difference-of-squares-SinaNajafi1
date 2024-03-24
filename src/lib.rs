019022be7dd262b40e1bd8f47f792556ca8d2c8a
Sina Najafi <148863702+SinaNajafi1@users.noreply.github.com>
2024-03-24 12:11:16 -0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,16 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    unimplemented!("square of sum of 1...{n}")
+    let sum_of_n: u32 = (1..=n).sum();
+    let result = sum_of_n * sum_of_n;
+    result
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    unimplemented!("sum of squares of 1...{n}")
+    let sum_of_squares_of_n: u32 = (1..=n).map(|s| s * s).sum();
+    sum_of_squares_of_n
 }
 
 pub fn difference(n: u32) -> u32 {
-    unimplemented!("difference between square of sum of 1...{n} and sum of squares of 1...{n}")
+    let a = square_of_sum(n);
+    let b = sum_of_squares(n);
+    a - b
 }
